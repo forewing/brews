d379267e61b2f9981426f32d4a9dc7c23042506d
forewing <13747187+forewing@users.noreply.github.com>
2022-04-08 11:17:13 -0400

    add yt-dlp-is-youtube-dl

---
+++ Formula/yt-dlp-is-youtube-dl.rb
@@ -0,0 +1,22 @@
+class YtDlpIsYoutubeDl < Formula
+  desc "Create youtube-dl linked to yt-dlp"
+  homepage "https://github.com/yt-dlp/yt-dlp"
+  url "file:///dev/null"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  version "1.0.0"
+  license "The Unlicense"
+  conflicts_with "youtube-dl", because: "naming conflict"
+
+  depends_on "yt-dlp"
+
+  def install
+    yt_dlp_path = `which yt-dlp`.strip
+    system yt_dlp_path, "--version"
+
+    system "ln", "-s", yt_dlp_path, "youtube-dl"
+    system "./youtube-dl", "--version"
+
+    bin.install "youtube-dl"
+  end
+
+end
