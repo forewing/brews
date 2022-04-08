7f9e03f88db3a079fc3d367cab97656af5dc3eb0
forewing <13747187+forewing@users.noreply.github.com>
2022-04-08 11:25:15 -0400

    rename youtube-dl

--- Formula/youtube-dl.rb
+++ Formula/youtube-dl.rb
@@ -1,12 +1,12 @@
-class YtDlpIsYoutubeDl < Formula
+class YoutubeDl < Formula
   desc "Create youtube-dl linked to yt-dlp"
   homepage "https://github.com/yt-dlp/yt-dlp"
   url "file:///dev/null"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   version "1.0.0"
   license "The Unlicense"
-  conflicts_with "youtube-dl", because: "naming conflict"
 
+  conflicts_with "youtube-dl", because: "naming conflict"
   depends_on "yt-dlp"
 
   def install
