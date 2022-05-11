6fa6bd356b3297ce0a15cc27a486624e2f96973a
forewing <13747187+forewing@users.noreply.github.com>
2022-05-11 16:38:20 -0400

    go-latest 1.18.2

--- Formula/go-latest.rb
+++ Formula/go-latest.rb
@@ -1,8 +1,8 @@
 class GoLatest < Formula
   desc "Latest Go, fuck the 100000000 deps"
   homepage "https://go.dev/"
-  url "https://go.dev/dl/go1.18.1.src.tar.gz"
-  sha256 "efd43e0f1402e083b73a03d444b7b6576bb4c539ac46208b63a916b69aca4088"
+  url "https://go.dev/dl/go1.18.2.src.tar.gz"
+  sha256 "2c44d03ea2c34092137ab919ba602f2c261a038d08eb468528a3f3a28e5667e2"
   license "BSD-3-Clause"
   head "https://go.googlesource.com/go.git", branch: "master"
   conflicts_with "go", because: "it sucks, runs years of CI for a minor update"
